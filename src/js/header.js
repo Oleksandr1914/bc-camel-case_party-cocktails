96fb3e1b7c501f29bf40b3bc8625f7d9279d5a0e
Nikita Taranenko <soomraq.x@gmail.com>
2022-10-28 21:12:17 +0300

    small fix for header.js

--- src/js/header.js
+++ src/js/header.js
@@ -10,19 +10,13 @@ burger.addEventListener('click', () => {
 });
 
 // Scroll Header ------------------------
-
-// When the user scrolls the page, execute myFunction
 window.onscroll = function () {
   myFunction();
 };
 
-// Get the header
-var header = document.querySelector('header');
+const header = document.querySelector('header');
+const sticky = header.offsetTop;
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add('sticky');
