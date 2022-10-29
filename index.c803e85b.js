4fa454894d0a5574bdebbfdb8591c5fab815c25f
Oleksandr1914 <108628967+Oleksandr1914@users.noreply.github.com>
2022-10-29 16:34:17 +0000

    Deploying to gh-pages from @ Oleksandr1914/bc-camel-case_party-cocktails@2885f62dc149493f6f98859dbde387c9fa80bdf7 🚀

--- index.c803e85b.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".menu__icon"),t=document.querySelector(".menu__content");e.addEventListener("click",(()=>{document.body.classList.toggle("_lock"),e.classList.toggle("_active"),t.classList.toggle("_active")})),window.onscroll=function(){window.pageYOffset>o?c.classList.add("sticky"):c.classList.remove("sticky")};const c=document.querySelector("header"),o=c.offsetTop;
-//# sourceMappingURL=index.c803e85b.js.map
